df5ab14bff7e49f62d04c79b1652242600c137ba
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-08-27 19:44:13 +0300

    handling errors

--- app.py
+++ app.py
@@ -13,25 +13,29 @@ def predict():
     prediction = []
     errors = []
     if request.method == 'POST':
-        read_ss = int(request.values.get('read_ss'))
-        ever_alternative = int(request.values.get('ever_alternative'))
-        gpa = float(request.values.get('gpa'))
-        african_american = int(request.values.get('african_american')) 
+        try:
+            read_ss = int(request.values.get('read_ss'))
+            ever_alternative = int(request.values.get('ever_alternative'))
+            gpa = float(request.values.get('gpa'))
+            african_american = int(request.values.get('african_american')) 
 
-        print(read_ss, ever_alternative, gpa, african_american)
-        # create numpy array
-        if read_ss < 0 or read_ss > 100:
-            errors.append('read_ss must be between 0 and 100')
-        if ever_alternative < 0 or ever_alternative > 1:
-            errors.append('ever_alternative must be between 0 and 1')
-        if gpa < 0 or gpa > 4:
-            errors.append('gpa must be between 0 and 4')
+            print(read_ss, ever_alternative, gpa, african_american)
+            # create numpy array
+            if read_ss < 0 or read_ss > 100:
+                errors.append('read_ss must be between 0 and 100')
+            if ever_alternative < 0 or ever_alternative > 1:
+                errors.append('ever_alternative must be between 0 and 1')
+            if gpa < 0 or gpa > 4:
+                errors.append('gpa must be between 0 and 4')
 
-        if african_american != 0:
-            african_american = 1
+            if african_american != 0:
+                african_american = 1
 
-        prediction = model.predict([[read_ss, ever_alternative, gpa, african_american]])
-    
+            prediction = model.predict([[read_ss, ever_alternative, gpa, african_american]])
+        
+        except:
+            return flask.jsonify({'error': 'Invalid input'});
+        
     prediction = list(prediction)
     answer = prediction[0]
     if len(errors) > 0:
