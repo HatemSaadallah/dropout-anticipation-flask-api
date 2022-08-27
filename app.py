57b622f21d1fdde76b67a3db81ade5570a552d16
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2022-08-27 21:49:48 +0300

    handling errors

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, make_response
 import joblib
 import json
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 model = joblib.load(open('model.pkl', 'rb'))
 
 
@@ -12,41 +13,49 @@ model = joblib.load(open('model.pkl', 'rb'))
 def predict():
     prediction = []
     errors = []
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
     if request.method == 'POST':
         try:
-            try:
-                read_ss = int(request.values.get('read_ss'))
-            except ValueError:
-                return json.dumps({'error': 'read_ss is not a number'})
-            try:
-                ever_alternative = int(request.values.get('ever_alternative'))
-            except ValueError:
-                return json.dumps({'error': 'ever_alternative is not a number'})
-            try:
-                gpa = float(request.values.get('gpa'))
-            except ValueError:
-                return json.dumps({'error': 'gpa is not a number'})
-            try:
-                african_american = int(request.values.get('african_american')) 
-            except ValueError:
-                return json.dumps({'error': 'african_american is not a number'})
-                
-            print(read_ss, ever_alternative, gpa, african_american)
-            # create numpy array
-            if read_ss < 0 or read_ss > 100:
-                errors.append('read_ss must be between 0 and 100')
-            if ever_alternative < 0 or ever_alternative > 1:
-                errors.append('ever_alternative must be between 0 and 1')
-            if gpa < 0 or gpa > 4:
-                errors.append('gpa must be between 0 and 4')
+            read_ss = int(request.values.get('read_ss'))
+        except ValueError:
+            response.status_code = 400
+            response.data = json.dumps({"error": "read_ss is not a number"})
+            return response
+        try:
+            ever_alternative = int(request.values.get('ever_alternative'))
+        except ValueError:
+            response.status_code = 400
+            response.data = json.dumps({"error": "ever_alternative is not a number"})
+            return response
+        try:
+            gpa = float(request.values.get('gpa'))
+        except ValueError:
+            response.status_code = 400
+            response.data = json.dumps({"error": "gpa is not a number"})
+            return response
+        try:
+            african_american = int(request.values.get('african_american')) 
+        except ValueError:
+            response.status_code = 400
+            response.data = json.dumps({"error": "african_american is not a number"})
+            return response
 
-            if african_american != 0:
-                african_american = 1
+        print(read_ss, ever_alternative, gpa, african_american)
+        # create numpy array
+        if read_ss < 0 or read_ss > 100:
+            errors.append('read_ss must be between 0 and 100')
+        if ever_alternative < 0 or ever_alternative > 1:
+            errors.append('ever_alternative must be between 0 and 1')
+        if gpa < 0 or gpa > 4:
+            errors.append('gpa must be between 0 and 4')
 
-            prediction = model.predict([[read_ss, ever_alternative, gpa, african_american]])
-        
-        except:
-            return {'status': 400, 'error': 'Invalid input'};
+        if african_american != 0:
+            african_american = 1
+
+        prediction = model.predict([[read_ss, ever_alternative, gpa, african_american]])
         
     prediction = list(prediction)
     answer = prediction[0]
